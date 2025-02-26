ab44b6f80e399fcdba2b6dd3179d3774b1704747
RoMANzhula <roman1985al@ukr.net>
2025-02-26 23:14:39 +0200

    Add User model - user-service

---
+++ user-service/src/main/java/org/romanzhula/user_service/models/User.java
@@ -0,0 +1,37 @@
+package org.romanzhula.user_service.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id", unique = true, updatable = false, nullable = false)
+    private UUID id;
+
+    @Column(name = "nickname", unique = true, nullable = false)
+    private String username;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(name = "phone_number", unique = true, nullable = false)
+    private String phone;
+
+}
