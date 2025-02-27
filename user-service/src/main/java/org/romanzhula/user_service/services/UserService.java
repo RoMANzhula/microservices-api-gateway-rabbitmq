22e79016eeeb326ee0d1ce56ee247497fd379aef
RoMANzhula <roman1985al@ukr.net>
2025-02-27 22:21:05 +0200

    Up UserService - user-service - add addNewUser() method with rabbitmq

--- user-service/src/main/java/org/romanzhula/user_service/services/UserService.java
+++ user-service/src/main/java/org/romanzhula/user_service/services/UserService.java
@@ -2,8 +2,11 @@ package org.romanzhula.user_service.services;
 
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.romanzhula.user_service.models.User;
 import org.romanzhula.user_service.repositories.UserRepository;
+import org.romanzhula.user_service.requests.UserRequest;
 import org.romanzhula.user_service.responses.UserResponse;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -15,6 +18,8 @@ import java.util.UUID;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final RabbitTemplate rabbitTemplate;
+
 
     @Transactional(readOnly = true)
     public List<UserResponse> getAll() {
@@ -44,6 +49,24 @@ public class UserService {
                 ))
                 .orElseThrow(() -> new EntityNotFoundException("User not found with id: " + userId))
         ;
+    }
+
+    @Transactional
+    public String addNewUser(UserRequest userRequest) {
+        User user = User.builder()
+                .username(userRequest.getUsername())
+                .firstName(userRequest.getFirstName())
+                .lastName(userRequest.getLastName())
+                .email(userRequest.getEmail())
+                .phone(userRequest.getPhone())
+                .build()
+        ;
+
+        userRepository.save(user);
+
+        rabbitTemplate.convertAndSend("user-created-queue", user);
+
+        return "User added successfully!";
     }
 
 }
