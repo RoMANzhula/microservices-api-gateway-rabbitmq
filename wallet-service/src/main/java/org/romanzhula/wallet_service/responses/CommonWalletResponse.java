76ed03637742c1ca6697c9e714606ee00e11af4d
RoMANzhula <roman1985al@ukr.net>
2025-02-28 20:02:48 +0200

    Add CommonWalletResponse - wallet-service

---
+++ wallet-service/src/main/java/org/romanzhula/wallet_service/responses/CommonWalletResponse.java
@@ -0,0 +1,17 @@
+package org.romanzhula.wallet_service.responses;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+
+@Getter
+@AllArgsConstructor
+public class CommonWalletResponse {
+
+    private UUID userId;
+    private BigDecimal balance;
+
+}
