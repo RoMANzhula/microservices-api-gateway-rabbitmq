3162b8b2871d7c4300ebdc8495a19e30fc2e38d0
RoMANzhula <roman1985al@ukr.net>
2025-03-01 19:18:14 +0200

    Add RabbitmqConfig - wallet-service

---
+++ wallet-service/src/main/java/org/romanzhula/wallet_service/configurations/RabbitmqConfig.java
@@ -0,0 +1,14 @@
+package org.romanzhula.wallet_service.configurations;
+
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+@Getter
+@Configuration
+public class RabbitmqConfig {
+
+    @Value("${rabbitmq.queue.name.user-created}")
+    private String queueUserCreated;
+
+}
